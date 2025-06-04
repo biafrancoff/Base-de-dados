4df930b58cab9aff1fd231246126df578003bb93
biafrancoff <beatrizff@id.uff>
2025-06-03 23:59:04 -0300

    base de dados atualizada

--- base.R
+++ base.R
@@ -1,5 +1,8 @@
-z=1
-y=2
-sum(z,y)
-y-z
-y+z
+install.packages("readxl")
+library(readxl)
+base_final <- read_excel("C:/Users/scrw/Downloads/base_final.xlsx")
+View(base_final)
+library(dplyr)
+baseAR = base_final |> 
+  filter(NO_MUNICIPIO == "Angra dos Reis")
+View(baseAR)
