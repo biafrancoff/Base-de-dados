3ead02e25f7553762f43e2893bbf8aa3a2235ffd
biafrancoff <beatrizff@id.uff>
2025-06-04 00:07:29 -0300

    base atualizada agora

--- base.R
+++ base.R
@@ -3,9 +3,11 @@ install.packages("readxl")
 library(readxl)
 library(dplyr)
 
+
 base_final <- read_excel("C:/Users/scrw/Downloads/base_final.xlsx")
 View(base_final)
 
+#Filtrando somente o município de Angra dos Reis
 baseAR = base_final |>
   filter(NO_MUNICIPIO == "Angra dos Reis")
 View(baseAR)
