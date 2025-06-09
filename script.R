0ecf27e8ecc7409acd7c2910aa097c49a2b454ab
Beatriz Franco de Freitas <beatrizff@id.uff.br>
2025-06-09 16:03:47 -0300

    Update script.R

--- script.R
+++ script.R
@@ -1,13 +1,1 @@
-# Instalar pacotes e carregar dados
-install.packages("readxl")
-library(readxl)
-library(dplyr)
 
-
-base_final <- read_excel("C:/Users/scrw/Downloads/base_final.xlsx")
-View(base_final)
-
-#Filtrando somente o município de Angra dos Reis
-baseAR = base_final |>
-  filter(NO_MUNICIPIO == "Angra dos Reis")
-View(baseAR)
